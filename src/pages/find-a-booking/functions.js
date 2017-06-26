f163c5747950f95967bcc7e7364cb869fb07c56e
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-06-26 22:20:19 +0100

    Set up search results

--- src/pages/find-a-booking/functions.js
+++ src/pages/find-a-booking/functions.js
@@ -1,8 +1,51 @@
 'use strict';
 
+const apiUrl = (process.env.API_URL || 'http://localhost:3000') + '/api';
+const got = require('got');
 const template = require('./template.marko');
 
 function get(req, res) {
+  const search = req.query.search;
+  if (search) {
+    return got(`${apiUrl}/Bookings`, {
+      json: true,
+      query: {
+        filter: JSON.stringify({
+          where: {
+            or: [
+              {
+                name: {
+                  like: search,
+                  options: 'i'
+                }
+              },
+              {
+                description: {
+                  like: search,
+                  options: 'i'
+                }
+              }
+            ]
+          },
+          include: [
+            {
+              relation: 'room',
+              scope: {
+                include: 'location'
+              }
+            }
+          ]
+        })
+      }
+    }).then(response => {
+      const results = response.body;
+      console.log(results)
+      const resultText = (results.length === 1 ? 'result' : 'results');
+      return template.render({results, resultText}, res);
+    }).catch(err => {
+      console.log(err);
+    });
+  }
   template.render({}, res);
 }
 
